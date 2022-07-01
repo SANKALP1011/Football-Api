f7e06af48de05133bd58fa64d29536291c309200
SANKALP1011 <tigerishan426@gmail.com>
2022-07-01 15:56:37 +0530

    ..issue

--- Controller/clubs.controller.js
+++ Controller/clubs.controller.js
@@ -4,18 +4,18 @@ const {FetchClubs} = require("../Service/club.service")
 
 module.exports = {
   GetAllClubs: async (req,res)=>{
-       FetchClubs((err,results)=>{
-        if(err){
-          console.log(err)
-          return res.status(200).json({
-            Success: "No",
-            message: err
-            })
-        }
-        else{
-             res.send(results)
-        }
-       }) 
+      //  FetchClubs((err,results)=>{
+      //   if(err){
+      //     console.log(err)
+      //     return res.status(200).json({
+      //       Success: "No",
+      //       message: err
+      //       })
+      //   }
+      //   else{
+      //        res.send(results)
+      //   }
+      //  }) 
   },
   GetClubsByCountry: async (req,res)=>{
     
--- Service/club.service.js
+++ Service/club.service.js
@@ -12,6 +12,5 @@ module.exports = {
             callback(null,results)
         }
        })
-        
-    }
+ }
 }
