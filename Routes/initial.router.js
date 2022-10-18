8000515012267020adf86d15115f92a6de6e00ec
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2022-10-18 21:28:30 +0530

    ..

--- Routes/initial.router.js
+++ Routes/initial.router.js
@@ -2,10 +2,13 @@ const express = require("express");
 const InitailRoute = express.Router();
 
 InitailRoute.get("/", (req, res) => {
+  res.setHeader("Content-type", "text/html");
   res.write(
-    "Welcome to the authenticated football api, use postman to test it out."
+    "<h1>Welcome to the authenticated football api, use postman to test it out</h1><br>"
   );
-  res.write("Currently, documnetation has not been added for this api.");
+  res.write(
+    "<h2>Currently, documnetation has not been added for this api</h2>"
+  );
   res.send();
 });
 module.exports = InitailRoute;
