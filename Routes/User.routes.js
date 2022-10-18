0d05c63d1b32d076b2e13cbb3a515f5aef022136
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2022-10-18 20:06:39 +0530

    Jwt issue fixed

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserRouter = express.Router();
 const { CreateUser, Login } = require("../Controller/user.controller");
+const token = require("../Middleware/AuthToken");
 
 UserRouter.post("/Register", CreateUser);
 UserRouter.post("/Login", Login);
--- app.js
+++ app.js
@@ -4,64 +4,61 @@ const path = require("path");
 const swaggerJsDocs = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const User = require("./Routes/User.routes");
-const League = require("./Routes/league.router")
-const Clubs = require("./Routes/clubs.router")
-const Players = require("./Routes/players.router")
-const Trophie = require("./Routes/trophies.router")
-const db = require("./Model/index")
+const League = require("./Routes/league.router");
+const Clubs = require("./Routes/clubs.router");
+const Players = require("./Routes/players.router");
+const Trophie = require("./Routes/trophies.router");
 
-const port = process.env.PORT || "3004"
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-require('dotenv').config({ 
-    path: path.resolve(__dirname, './.env') 
-  })
+const port = process.env.PORT || "3004";
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require("dotenv").config({
+  path: path.resolve(__dirname, "./.env"),
+});
 
-  const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Football Api',
-      version: '1.0.0',
-      description: 'This is an user aunthenticated football api where once the user registers and logins with the same  credentials , user would get a access token which ca help user to access the football api routes.',
-      license: {
-        name: 'Licensed Under MIT',
-        url: 'https://spdx.org/licenses/MIT.html',
-      },
-      contact: {
-        name: 'Sankalp',
-        url: 'https://portfolio-site-sankalp1011.vercel.app/',
-      },
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Football Api",
+    version: "1.0.0",
+    description:
+      "This is an user aunthenticated football api where once the user registers and logins with the same  credentials , user would get a access token which ca help user to access the football api routes.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
     },
-    servers: [
-      {
-        url: "http://localhost:3004",
-        description: 'Production Server',
-      }
-      ],
-  };
-
-const swaggerDocs = 
-{
-  swaggerDefinition,
-  apis: [`${__dirname}/Routes/*.js`]
-}
-
-var options = {
-  customCss: '.swagger-ui .topbar { display: none }'
+    contact: {
+      name: "Sankalp",
+      url: "https://portfolio-site-sankalp1011.vercel.app/",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:3004",
+      description: "Production Server",
+    },
+  ],
 };
 
-const swaggerConfifg = swaggerJsDocs(swaggerDocs)
+const swaggerDocs = {
+  swaggerDefinition,
+  apis: [`${__dirname}/Routes/*.js`],
+};
 
-app.use("/v1",swaggerUi.serve,swaggerUi.setup(swaggerConfifg,options))
+var options = {
+  customCss: ".swagger-ui .topbar { display: none }",
+};
 
-app.use(Clubs)
-app.use(User)
-app.use(League)
-app.use(Players)
-app.use(Trophie)
+const swaggerConfifg = swaggerJsDocs(swaggerDocs);
 
+app.use("/v1", swaggerUi.serve, swaggerUi.setup(swaggerConfifg, options));
 
+app.use(Clubs);
+app.use(User);
+app.use(League);
+app.use(Players);
+app.use(Trophie);
 
-app.listen(port,(err)=>{
-    console.log("server up and running")
-})
+app.listen(port, (err) => {
+  console.log("server up and running");
+});
